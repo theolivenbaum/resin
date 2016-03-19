8ba96293482bbf3c53d22545890646b36aa3581f
Marcus Lager <marcus.lager@avensia.se>
2016-03-19 09:35:53 +0100

    cli helpers

--- Cli/rn-write.bat
+++
@@ -1,2 +0,0 @@
-rmdir /s /q c:\temp\resin\wikipedia
-rn.exe write --file e:\wikipedia\wikipedia_resin.json --dir c:\temp\resin\wikipedia --skip 0 --take 100000
---
+++ rnw.bat
@@ -0,0 +1,1 @@
+Resin.WikipediaJsonParser\bin\Release\rnw.exe %*
