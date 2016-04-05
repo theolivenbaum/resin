b0ae33cf2ab56cf7876d8b9fb8d93e4f3d8c1933
Marcus Lager <marcus.lager@avensia.se>
2016-04-05 22:25:24 +0200

    bug fix: don't analyze fields that start with underscore

--- Resin/QueryParser.cs
+++ Resin/QueryParser.cs
@@ -3,6 +3,85 @@ using System.Linq;
 
 namespace Resin
 {
+    public class QueryInterpreter
+    {
+        private Term _root;
+        private readonly string _text;
+        private int _start;
+
+
+        private readonly char[] _fieldOperators = { '+','-' };
+        private readonly char[] _termOperators = { '~', '*' };
+
+
+        public Term Terms { get { return _root; } }
+
+        private string _field;
+        private string _token;
+        private bool _and;
+        private bool _not;
+        private bool _prefix;
+        private bool _fuzzy;
+
+        public QueryInterpreter(string text)
+        {
+            _text = text;
+        }
+
+        public void Step(int index)
+        {
+            var c = _text[index];
+            var type = Analyze(c);
+
+            if (type == InputType.FieldOperator)
+            {
+                if (c == '+') _and = true;
+                else if (c == '-') _not = true;
+            }
+            else if (type == InputType.TermOperator)
+            {
+                if (c == '~') _fuzzy = true;
+                else if (c == '*') _prefix = true;
+            }
+            else if (type == InputType.EndOfField)
+            {
+                _field = _text.Substring(_start, index - _start);
+            }
+        }
+
+        
+
+        private InputType Analyze(char c)
+        {
+            if (_fieldOperators.Contains(c)) return InputType.FieldOperator;
+            if (_termOperators.Contains(c)) return InputType.TermOperator;
+            if (char.IsLetterOrDigit(c)) return InputType.Data;
+            if(c==':') return InputType.EndOfField;
+            return InputType.Other;
+        }
+
+        private enum InputType
+        {
+            TermOperator,
+            FieldOperator, 
+            EndOfField,
+            Data, 
+            Other
+        }
+
+        private enum State
+        {
+            Start,
+            FieldOperator,
+            Field,
+            EndOfField,
+            Token,
+            TokenOperator,
+        }
+    }
+
+
+
     public class QueryParser
     {
         private readonly IAnalyzer _analyzer;
@@ -14,6 +93,28 @@ namespace Resin
 
         public IEnumerable<Term> Parse(string query)
         {
+            //var parser = new QueryInterpreter(query);
+            //for (int i = 0; i < query.Length; i++)
+            //{
+            //    parser.Step(i);
+            //}
+            //var terms = parser.Terms;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             var termCount = 0;
             foreach (var term in query.Split(' '))
             {
@@ -50,9 +151,14 @@ namespace Resin
                     fuzzy = true;
                 }
 
+                if (field.StartsWith("_"))
+                {
+                    yield return new Term(field, value) { And = and, Not = not, Prefix = prefix, Fuzzy = fuzzy, Similarity = 0.75f };
+                    yield break;
+                }
                 foreach (var token in _analyzer.Analyze(value))
                 {
-                    yield return new Term {Field = field, Token = token, And = and, Not = not, Prefix = prefix, Fuzzy = fuzzy, Similarity = 0.75f};
+                    yield return new Term(field, token){And = and, Not = not, Prefix = prefix, Fuzzy = fuzzy, Similarity = 0.75f};
                 }
             }
         }
