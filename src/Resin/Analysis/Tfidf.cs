d558ff891a6d87a57e9cccd9fd73dee36ced97d2
Marcus Lager <marcuslager@gmail.com>
2017-02-14 12:45:55 +0100

    classic tf-idf

--- src/Resin/Analysis/Tfidf.cs
+++ src/Resin/Analysis/Tfidf.cs
@@ -20,20 +20,21 @@ namespace Resin.Analysis
         }
 
         /// <summary>
-        /// Create scorer.
-        /// https://lucene.apache.org/core/4_0_0/core/org/apache/lucene/search/similarities/TFIDFSimilarity.html
+        /// Create scorer. 
+        /// On tf-idf: 
+        /// https://lucene.apache.org/core/6_4_1/core/org/apache/lucene/search/similarities/TFIDFSimilarity.html 
+        /// https://en.wikipedia.org/wiki/Tf%E2%80%93idf
         /// </summary>
         /// <param name="docsInCorpus"></param>
         /// <param name="docsWithTerm"></param>
         public Tfidf(int docsInCorpus, int docsWithTerm)
         {
-            _idf = Math.Log10(docsInCorpus / (double)docsWithTerm + 1) + 1;
+            _idf = 1 + Math.Log10(docsInCorpus / (double)docsWithTerm + 1);
         }
 
         public void Score(DocumentScore doc)
         {
-            var tf = Math.Sqrt(doc.TermCount);
-            doc.Score = tf * _idf;
+            doc.Score = Math.Sqrt(doc.TermCount) * _idf;
         }
 
         public IScoringScheme CreateScorer(int docsInCorpus, int docsWithTerm)
