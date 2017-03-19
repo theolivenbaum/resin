1852195a04ad1378b50be59ad261b46d775f539d
Marcus Lager <marcuslager@gmail.com>
2017-03-19 19:41:06 +0100

    new document reader/writer

---
+++ src/Resin/IO/Read/DocumentReader.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Resin.IO.Write;
+
+namespace Resin.IO.Read
+{
+    public class DocumentReader : IDisposable
+    {
+        private readonly Stream _stream;
+        private long _position;
+
+        public DocumentReader(Stream stream)
+        {
+            _stream = stream;
+            _position = 0;
+        }
+
+        public IEnumerable<Document> Get(IEnumerable<BlockInfo> blocks)
+        {
+            return blocks.Select(Get);
+        }
+
+        private Document Get(BlockInfo info)
+        {
+            var distance = info.Position - _position;
+            
+            if (distance > 0)
+            {
+                _stream.Seek(distance, SeekOrigin.Current);
+            }
+
+            var buffer = new byte[info.Length];
+
+            _stream.Read(buffer, 0, buffer.Length);
+
+            _position = info.Position + info.Length;
+            
+            return Deserialize(buffer);
+        }
+
+        private Document Deserialize(byte[] data)
+        {
+            using (var stream = new MemoryStream(data))
+            {
+                return (Document)GraphSerializer.Serializer.Deserialize(stream);
+            }
+        }
+
+        public void Dispose()
+        {
+            _stream.Dispose();
+        }
+    }
+}
---
+++ src/Resin/IO/Write/DocumentWriter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+
+namespace Resin.IO.Write
+{
+    public class DocumentWriter : IDisposable
+    {
+        private long _position;
+        private readonly Stream _stream;
+
+        public DocumentWriter(Stream stream)
+        {
+            _position = 0;
+            _stream = stream;
+        }
+
+        public BlockInfo Write(Document doc)
+        {
+            var bytes = Serialize(doc);
+            var info = new BlockInfo(_position, bytes.Length);
+
+            _stream.Write(bytes, 0, bytes.Length);
+            _position += bytes.Length;
+
+            return info;
+        }
+
+        private byte[] Serialize(Document doc)
+        {
+            using (var ms = new MemoryStream())
+            {
+                GraphSerializer.Serializer.Serialize(ms, doc);
+                return ms.ToArray();
+            }
+        }
+
+        public void Dispose()
+        {
+            _stream.Dispose();
+        }
+    }
+
+    public struct BlockInfo
+    {
+        public long Position;
+        public int Length;
+
+        public BlockInfo(long position, int length)
+        {
+            Position = position;
+            Length = length;
+        }
+    }
+}
