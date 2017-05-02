f1b5ce0cf0b034da0fc971c894ac578094c9828e
Marcus Lager <marcus.lager@avensia.se>
2017-05-02 13:07:57 +0200

    hashing

--- src/Resin/Sys/HashExtensions.cs
+++ src/Resin/Sys/HashExtensions.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Globalization;
 using System.Text;
 
 namespace Resin.Sys
@@ -15,24 +14,8 @@ namespace Resin.Sys
         public static UInt32 ToHash(this string text)
         {
             var bytes = Encoding.UTF8.GetBytes(text);
-            return new MurmurHash2UInt32Hack().Hash(bytes);
+            return mm.Hash(bytes);
         }
-
-        /// <summary>
-        /// Knuth hash. http://stackoverflow.com/questions/9545619/a-fast-hash-function-for-string-in-c-sharp
-        /// One could also use https://msdn.microsoft.com/en-us/library/system.security.cryptography.sha1.aspx
-        /// </summary>
-        /// <param name="text"></param>
-        /// <returns></returns>
-        //public static UInt64 ToHash(this string text)
-        //{
-        //    UInt64 hashedValue = 3074457345618258791ul;
-        //    for (int i = 0; i < text.Length; i++)
-        //    {
-        //        hashedValue += text[i];
-        //        hashedValue *= 3074457345618258799ul;
-        //    }
-        //    return hashedValue;
-        //}
+        private static MurmurHash2UInt32Hack mm = new MurmurHash2UInt32Hack();
     }
 }
