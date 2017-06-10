bc0eb474213d1ad5b227746da9d99b32ce68f57c
Marcus Lager <marcus.lager@avensia.se>
2017-06-10 20:45:50 +0200

    merge test

--- src/Resin.UnitTests/SearcherTests.cs
+++ src/Resin.UnitTests/SearcherTests.cs
@@ -190,50 +190,55 @@ namespace Tests
             }
         }
 
-//        [TestMethod]
-//        public void Can_merge()
-//        {
-//            var dir = CreateDir();
+        [TestMethod]
+        public void Can_merge()
+        {
+            var dir = CreateDir();
 
-//            var docs = new List<dynamic>
-//            {
-//                new {_id = "0", title = "Rambo First Blood" },
-//                new {_id = "1", title = "the rain man" },
-//                new {_id = "2", title = "the good, the bad and the ugly" }
-//            }.ToDocuments(primaryKeyFieldName: "_id");
+            var docs = new List<dynamic>
+            {
+                new {_id = "0", title = "Rambo First Blood" },
+                new {_id = "1", title = "the rain man" },
+                new {_id = "2", title = "the good, the bad and the ugly" }
+            }.ToDocuments(primaryKeyFieldName: "_id");
 
-//            var writer = new UpsertOperation(
-//                dir, new Analyzer(), compression: Compression.NoCompression, documents: docs);
-//            long indexName = writer.Write();
-//            writer.Dispose();
+            var moreDocs = new List<dynamic>
+{
+                new {_id = "3", title = "rambo 2" },
+                new {_id = "4", title = "rocky 2" },
+                new {_id = "5", title = "the raiders of the lost ark" },
+            }.ToDocuments(primaryKeyFieldName: "_id");
 
-//            var moreDocs = new List<dynamic>
-//{
-//                new {_id = "3", title = "rambo 2" },
-//                new {_id = "4", title = "rocky 2" },
-//                new {_id = "5", title = "the raiders of the lost ark" },
-//            }.ToDocuments(primaryKeyFieldName: "_id");
+            long indexName;
+            long indexName2;
 
-//            var writer2 = new UpsertOperation(
-//                dir, new Analyzer(), compression: Compression.NoCompression, documents: moreDocs);
-//            long indexName2 = writer2.Write();
-//            writer2.Dispose();
+            using (var writer = new UpsertOperation(
+                dir, new Analyzer(), compression: Compression.NoCompression, documents: docs))
+            {
+                indexName = writer.Write();
 
-//            using (var merge = new MergeOperation(dir))
-//                merge.Merge(Compression.NoCompression, "_id");
+                using(var writer2 = new UpsertOperation(
+                    dir, new Analyzer(), compression: Compression.NoCompression, documents: moreDocs))
+                {
+                    indexName2 = writer2.Write();
+                }
+            }
 
-//            using (var searcher = new Searcher(dir))
-//            {
-//                var result = searcher.Search("title:rambo");
+            using (var merge = new MergeOperation(dir))
+                merge.Merge(Compression.NoCompression, "_id");
 
-//                Assert.AreEqual(2, result.Total);
-//                Assert.AreEqual(2, result.Docs.Count);
+            using (var searcher = new Searcher(dir))
+            {
+                var result = searcher.Search("title:rambo");
 
-//                Assert.IsTrue(result.Docs.Any(d => d.Document.Fields["_id"].Value == "0"));
-//                Assert.IsTrue(result.Docs.Any(d => d.Document.Fields["_id"].Value == "3"));
+                Assert.AreEqual(2, result.Total);
+                Assert.AreEqual(2, result.Docs.Count);
 
-//            }
-//        }
+                Assert.IsTrue(result.Docs.Any(d => d.Document.Fields["_id"].Value == "0"));
+                Assert.IsTrue(result.Docs.Any(d => d.Document.Fields["_id"].Value == "3"));
+
+            }
+        }
 
     }
 }
