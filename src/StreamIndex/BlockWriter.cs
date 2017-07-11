c3b099eedb85cfe0f1e42711a1b53a4e8ac51169
Marcus Lager <marcuslager@gmail.com>
2017-07-11 07:44:06 +0200

    use stream's position

--- src/StreamIndex/BlockWriter.cs
+++ src/StreamIndex/BlockWriter.cs
@@ -7,29 +7,28 @@ namespace StreamIndex
     {
         protected abstract byte[] Serialize(T block);
 
-        private long _position;
         private readonly Stream _stream;
 
         public Stream Stream { get { return _stream; } }
 
         public BlockWriter(Stream stream)
         {
-            _position = stream.Position;
             _stream = stream;
         }
 
         public BlockInfo Write(T block)
         {
+            var pos = _stream.Position;
+
             var bytes = Serialize(block);
 
-            var info = new BlockInfo(_position, bytes.Length);
+            var info = new BlockInfo(pos, bytes.Length);
 
             if (info.Length < 1)
                 throw new InvalidOperationException(
                     string.Format("invalid length {0}", info.Length));
 
             _stream.Write(bytes, 0, bytes.Length);
-            _position += bytes.Length;
 
             return info;
         }
