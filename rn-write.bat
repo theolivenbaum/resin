bebde13a78b801399ac860f090eceff60dc9d0f4
Marcus Lager <marcuslager@gmail.com>
2017-07-13 18:51:06 +0200

    write project gutenberg in batches

--- rn-write.bat
+++ rn-write.bat
@@ -1,23 +1,21 @@
-call rn write --file "c:\temp\pg\docs0.json" --dir c:\temp\resin\pg --pk url --skip 0 --take 2000
-call rn write --file "c:\temp\pg\docs0.json" --dir c:\temp\resin\pg --pk url --skip 2000 --take 2000
-call rn write --file "c:\temp\pg\docs0.json" --dir c:\temp\resin\pg --pk url --skip 4000 --take 2000
-call rn write --file "c:\temp\pg\docs0.json" --dir c:\temp\resin\pg --pk url --skip 6000 --take 2000
-call rn write --file "c:\temp\pg\docs0.json" --dir c:\temp\resin\pg --pk url --skip 8000 --take 2000
+call rn write --file c:\temp\636326999602241674.dtbl.json --dir c:\temp\resin_data\pg --pk url --skip 0 --take 1000
+call rn write --file c:\temp\636326999602241674.dtbl.json --dir c:\temp\resin_data\pg --pk url --skip 1000 --take 1000
+call rn write --file c:\temp\636326999602241674.dtbl.json --dir c:\temp\resin_data\pg --pk url --skip 2000 --take 1000
+call rn write --file c:\temp\636326999602241674.dtbl.json --dir c:\temp\resin_data\pg --pk url --skip 3000 --take 1000
+call rn write --file c:\temp\636326999602241674.dtbl.json --dir c:\temp\resin_data\pg --pk url --skip 4000 --take 1000
+call rn write --file c:\temp\636326999602241674.dtbl.json --dir c:\temp\resin_data\pg --pk url --skip 5000 --take 1000
+call rn write --file c:\temp\636326999602241674.dtbl.json --dir c:\temp\resin_data\pg --pk url --skip 6000 --take 1000
+call rn write --file c:\temp\636326999602241674.dtbl.json --dir c:\temp\resin_data\pg --pk url --skip 7000 --take 1000
+call rn write --file c:\temp\636326999602241674.dtbl.json --dir c:\temp\resin_data\pg --pk url --skip 8000 --take 1000
+call rn write --file c:\temp\636326999602241674.dtbl.json --dir c:\temp\resin_data\pg --pk url --skip 9000 --take 1000
+call rn write --file c:\temp\636326999602241674.dtbl.json --dir c:\temp\resin_data\pg --pk url --skip 10000 --take 1000
+call rn write --file c:\temp\636326999602241674.dtbl.json --dir c:\temp\resin_data\pg --pk url --skip 11000 --take 1000
+call rn write --file c:\temp\636326999602241674.dtbl.json --dir c:\temp\resin_data\pg --pk url --skip 12000 --take 1000
+call rn write --file c:\temp\636326999602241674.dtbl.json --dir c:\temp\resin_data\pg --pk url --skip 13000 --take 1000
+call rn write --file c:\temp\636326999602241674.dtbl.json --dir c:\temp\resin_data\pg --pk url --skip 14000 --take 1000
+call rn write --file c:\temp\636326999602241674.dtbl.json --dir c:\temp\resin_data\pg --pk url --skip 15000 --take 1000
+call rn write --file c:\temp\636326999602241674.dtbl.json --dir c:\temp\resin_data\pg --pk url --skip 16000 --take 1000
+call rn write --file c:\temp\636326999602241674.dtbl.json --dir c:\temp\resin_data\pg --pk url --skip 17000 --take 1000
+call rn write --file c:\temp\636326999602241674.dtbl.json --dir c:\temp\resin_data\pg --pk url --skip 18000 --take 1000
+call rn write --file c:\temp\636326999602241674.dtbl.json --dir c:\temp\resin_data\pg --pk url --skip 19000 --take 1000
 
-call rn write --file "c:\temp\pg\docs1.json" --dir c:\temp\resin\pg --pk url --skip 0 --take 2000
-call rn write --file "c:\temp\pg\docs1.json" --dir c:\temp\resin\pg --pk url --skip 2000 --take 2000
-call rn write --file "c:\temp\pg\docs1.json" --dir c:\temp\resin\pg --pk url --skip 4000 --take 2000
-call rn write --file "c:\temp\pg\docs1.json" --dir c:\temp\resin\pg --pk url --skip 6000 --take 2000
-call rn write --file "c:\temp\pg\docs1.json" --dir c:\temp\resin\pg --pk url --skip 8000 --take 2000
-
-call rn write --file "c:\temp\pg\docs2.json" --dir c:\temp\resin\pg --pk url --skip 0 --take 2000
-call rn write --file "c:\temp\pg\docs2.json" --dir c:\temp\resin\pg --pk url --skip 2000 --take 2000
-call rn write --file "c:\temp\pg\docs2.json" --dir c:\temp\resin\pg --pk url --skip 4000 --take 2000
-call rn write --file "c:\temp\pg\docs2.json" --dir c:\temp\resin\pg --pk url --skip 6000 --take 2000
-call rn write --file "c:\temp\pg\docs2.json" --dir c:\temp\resin\pg --pk url --skip 8000 --take 2000
-
-call rn write --file "c:\temp\pg\docs3.json" --dir c:\temp\resin\pg --pk url --skip 0 --take 2000
-call rn write --file "c:\temp\pg\docs3.json" --dir c:\temp\resin\pg --pk url --skip 2000 --take 2000
-call rn write --file "c:\temp\pg\docs3.json" --dir c:\temp\resin\pg --pk url --skip 4000 --take 2000
-call rn write --file "c:\temp\pg\docs3.json" --dir c:\temp\resin\pg --pk url --skip 6000 --take 2000
-call rn write --file "c:\temp\pg\docs3.json" --dir c:\temp\resin\pg --pk url --skip 8000 --take 2000
