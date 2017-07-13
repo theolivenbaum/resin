9195a3251a35dbdee10f099bfe453393b01b57eb
Marcus Lager <marcuslager@gmail.com>
2017-07-13 15:36:19 +0200

    remove silly API

--- src/Resin.UnitTests/SearcherTests.cs
+++ src/Resin.UnitTests/SearcherTests.cs
@@ -107,38 +107,6 @@ namespace Tests
         }
 
         [TestMethod]
-        public void Can_return_query_terms()
-        {
-            var dir = CreateDir();
-
-            var docs = new List<dynamic>
-{
-                new {_id = "0", title = "Rambo First Blood" },
-                new {_id = "1", title = "rambo 2" },
-                new {_id = "2", title = "rocky 2" },
-                new {_id = "3", title = "the raiders of the lost ark" },
-                new {_id = "4", title = "the rain man" },
-                new {_id = "5", title = "the good, the bad and the ugly" }
-            }.ToDocuments(primaryKeyFieldName: "_id");
-
-            var writer = new UpsertCommand(
-                dir, new Analyzer(), compression: Compression.NoCompression, documents: docs);
-            long indexName = writer.Write();
-            writer.Dispose();
-
-            using (var searcher = new Searcher(dir))
-            {
-                var result = searcher.Search("title:rambo blood title:first");
-
-                Assert.AreEqual(3, result.QueryTerms.Length);
-
-                Assert.IsTrue(result.QueryTerms.Any(d => d== "rambo"));
-                Assert.IsTrue(result.QueryTerms.Any(d => d == "first"));
-                Assert.IsTrue(result.QueryTerms.Any(d => d == "blood"));
-            }
-        }
-
-        [TestMethod]
         public void Can_search_two_index_segments()
         {
             var dir = CreateDir();
