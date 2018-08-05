469bdc29389f51c787a31171378fed03a999f5c2
Marcus Lager <marcus.lager@avensiastorefront.com>
2018-08-05 11:56:11 +0200

    parse html

--- src/Sir.HttpServer/Controllers/AddController.cs
+++ src/Sir.HttpServer/Controllers/AddController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.IO;
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Sir.HttpServer.Controllers
 {
@@ -7,6 +9,28 @@ namespace Sir.HttpServer.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Post(string url)
+        {
+            var str = GetWebString(url.StartsWith("https://") ? url : string.Format("https://{0}", url));
+            var decoded = WebUtility.HtmlDecode(str);
+            var parser = new YesNoParser('>', '<', new string[] { "script" });
+            var parsed = parser.Parse(decoded);
+
+            return RedirectToAction("¨Post", "IO", new { collectionId = "mycol" });
+        }
+
+        private static string GetWebString(string url)
+        {
+            var webRequest = WebRequest.Create(url);
+            using (var response = webRequest.GetResponse())
+            using (var content = response.GetResponseStream())
+            using (var reader = new StreamReader(content))
+            {
+                return reader.ReadToEnd();
+            }
         }
     }
 }
