7c1ff125f0849becb4f4aa39b377eb31c56f2879
kreeben <marcuslager@gmail.com>
2019-01-18 02:29:56 +0100

    reload ini values on file change

--- src/Sir/IniConfiguration.cs
+++ src/Sir/IniConfiguration.cs
@@ -5,21 +5,37 @@ namespace Sir
 {
     public class IniConfiguration : IConfigurationProvider
     {
-        private readonly IDictionary<string, string> _doc;
+        private IDictionary<string, string> _doc;
+        private readonly string _fileName;
 
         public IniConfiguration(string fileName)
         {
             _doc = new Dictionary<string, string>();
+            _fileName = Path.Combine(Directory.GetCurrentDirectory(), "sir.ini");
 
-            var path = Path.Combine(Directory.GetCurrentDirectory(), "sir.ini");
-            var file = File.ReadAllText(path).Split('\n');
+            OnFileChanged(null, null);
+
+            FileSystemWatcher watcher = new FileSystemWatcher();
+            watcher.Path = Directory.GetCurrentDirectory();
+            watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+            watcher.Filter = "*.ini";
+            watcher.Changed += new FileSystemEventHandler(OnFileChanged);
+            watcher.EnableRaisingEvents = true;
+        }
+
+        private void OnFileChanged(object sender, FileSystemEventArgs e)
+        {
+            var dic = new Dictionary<string, string>();
+            var file = File.ReadAllText(_fileName).Split('\n');
 
             foreach (var line in file)
             {
                 var segs = line.Split('=');
 
-                _doc.Add(segs[0].Trim(), segs[1].Trim());
+                dic.Add(segs[0].Trim(), segs[1].Trim());
             }
+
+            _doc = dic;
         }
 
         public string Get(string key)
