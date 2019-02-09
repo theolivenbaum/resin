06c297e420e7672303f5a0b549998b34eb506839
kreeben <marcuslager@gmail.com>
2019-02-09 22:30:26 +0100

    optional label&redirect

---
+++ src/Sir.HttpServer/Controllers/RedirectController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sir.HttpServer.Controllers
+{
+    public class RedirectController : UIController
+    {
+        public RedirectController(IConfigurationProvider config) : base(config)
+        {
+        }
+
+        [HttpGet("label-and-redirect")]
+        public ActionResult Index(string url)
+        {
+            return Redirect("https://" + System.Uri.UnescapeDataString(url));
+        }
+    }
+}
