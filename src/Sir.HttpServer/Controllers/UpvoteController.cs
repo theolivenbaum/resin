2a4d0af4aa5727156e98e607d0e6d246d0a1b087
kreeben <marcuslager@gmail.com>
2019-02-10 14:57:05 +0100

    fix upvote/redirect

---
+++ src/Sir.HttpServer/Controllers/UpvoteController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Web;
+
+namespace Sir.HttpServer.Controllers
+{
+    public class UpvoteController : UIController
+    {
+        public UpvoteController(IConfigurationProvider config) : base(config)
+        {
+        }
+
+        [HttpGet("upvote")]
+        public ActionResult Index(string collection, string q, string url, string OR, string AND, string[] fields)
+        {
+            ViewData["url"] = url;
+            ViewData["q"] = q;
+
+            return Redirect(Request.Headers["referer"]);
+        }
+    }
+}
