6de9b319537bf77b180b6e05753d4687482170aa
kreeben <marcuslager@gmail.com>
2019-02-10 18:03:33 +0100

    query cache

--- src/Sir.Postings/PostingsReader.cs
+++ src/Sir.Postings/PostingsReader.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 
@@ -13,10 +14,12 @@ namespace Sir.Postings
         public string ContentType => "application/postings";
 
         private readonly StreamRepository _data;
+        private readonly IDictionary<ulong, ResponseModel> _queryCache;
 
         public PostingsReader(StreamRepository data)
         {
             _data = data;
+            _queryCache = new Dictionary<ulong, ResponseModel>();
         }
 
         public async Task<ResponseModel> Read(string collectionId, HttpRequest request)
@@ -72,11 +75,19 @@ namespace Sir.Postings
                 }
                 else
                 {
-                    var query = Query.FromStream(buf);
+                    var queryHash = string.Format("{0}{1}{2}", Encoding.Unicode.GetString(buf), skip, take);
+                    var key = queryHash.ToHash();
 
-                    resultModel = await Reduce(collectionId.ToHash(), query, skip, take);
+                    if (!_queryCache.TryGetValue(key, out resultModel))
+                    {
+                        var query = Query.FromStream(buf);
 
-                    this.Log("executed query in {0}", timer.Elapsed);
+                        resultModel = await Reduce(collectionId.ToHash(), query, skip, take);
+
+                        _queryCache.TryAdd(key, resultModel);
+
+                        this.Log("executed query in {0}", timer.Elapsed);
+                    }
                 }
 
                 return resultModel;
