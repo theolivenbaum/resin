1f63e64befcda03b471047caff137a9975b09482
kreeben <marcuslager@gmail.com>
2019-04-23 16:35:05 +0200

    implement RocksDb as a IWriter plugin

---
+++ src/Sir.RocksDb/RocksDbReader.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using RocksDbSharp;
+
+namespace Sir.RocksDb
+{
+    public class RocksDbReader : IReader
+    {
+        private readonly IConfigurationProvider _config;
+
+        public string ContentType => "application/rocksdb+octet-stream";
+
+        public RocksDbReader(IConfigurationProvider config)
+        {
+            _config = config;
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public async Task<ResponseModel> Read(string collectionId, HttpRequest request)
+        {
+            var type = request.Query["type"].ToString();
+            var typeId = type.ToHash();
+            var path = Path.Combine(_config.Get("data_dir"), $"{typeId}.{collectionId}.rocks");
+            var id = BitConverter.GetBytes(long.Parse(request.Query["id"]));
+
+            var options = new DbOptions().SetCreateIfMissing(true);
+            byte[] response;
+
+            using (var db = RocksDbSharp.RocksDb.Open(options, path))
+            {
+                response = db.Get(id);
+            }
+
+            return new ResponseModel { Stream = new MemoryStream(response, false), MediaType = "application/rocksdb+octet-stream" };
+        }
+    }
+}
---
+++ src/Sir.RocksDb/RocksDbWriter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using RocksDbSharp;
+
+namespace Sir.RocksDb
+{
+    public class RocksDbWriter : IWriter
+    {
+        private readonly IConfigurationProvider _config;
+
+        public string ContentType => "application/rocksdb+octet-stream";
+
+        public RocksDbWriter(IConfigurationProvider config)
+        {
+            _config = config;
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public async Task<ResponseModel> Write(string collectionId, HttpRequest request)
+        {
+            var type = request.Query["type"].ToString();
+            var typeId = type.ToHash();
+            var path = Path.Combine(_config.Get("data_dir"), $"{typeId}.{collectionId}.rocks");
+
+            var id = request.Query.ContainsKey("id") ? 
+                BitConverter.GetBytes(long.Parse(request.Query["id"])) :
+                BitConverter.GetBytes(Guid.NewGuid().ToString().ToHash());
+
+            var requestStream = new MemoryStream();
+
+            await request.Body.CopyToAsync(requestStream);
+
+            var options = new DbOptions().SetCreateIfMissing(true);
+
+            using (var db = RocksDbSharp.RocksDb.Open(options, path))
+            {
+                db.Put(id, requestStream.ToArray());
+            }
+
+            var response = new MemoryStream();
+
+            await response.WriteAsync(id);
+
+            return new ResponseModel { Stream = response, MediaType = "application/octet-stream" };
+        }
+    }
+}
