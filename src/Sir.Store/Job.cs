247447f67bc89277c1ad6f72e825d80e03f51746
kreeben <marcuslager@gmail.com>
2019-04-30 19:27:58 +0200

    job class in separate file

---
+++ src/Sir.Store/Job.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Sir.Store
+{
+    public class Job
+    {
+        public string Collection { get; private set; }
+        public IEnumerable<IDictionary> Documents { get; private set; }
+        public bool Done { get; set; } 
+
+        public Job(string collection, IEnumerable<IDictionary> documents)
+        {
+            Collection = collection;
+            Documents = documents;
+        }
+    }
+}
--- src/Sir.Store/StoreWriter.cs
+++ src/Sir.Store/StoreWriter.cs
@@ -83,17 +83,4 @@ namespace Sir.Store
         {
         }
     }
-
-    public class Job
-    {
-        public string Collection { get; private set; }
-        public IEnumerable<IDictionary> Documents { get; private set; }
-        public bool Done { get; set; } 
-
-        public Job(string collection, IEnumerable<IDictionary> documents)
-        {
-            Collection = collection;
-            Documents = documents;
-        }
-    }
 }
