dbeb085fdddd0d6ae7e62932a9aa9e0530329125
kreeben <marcuslager@gmail.com>
2019-10-08 20:50:30 +0200

    natural language query parser

--- src/Sir.Store/Parsers/HttpQueryParser.cs
+++ src/Sir.Store/Parsers/HttpQueryParser.cs
@@ -11,11 +11,13 @@ namespace Sir.Store
     {
         private readonly SessionFactory _sessionFactory;
         private readonly IStringModel _model;
+        private readonly QueryParser _parser;
 
         public HttpQueryParser(SessionFactory sessionFactory, IStringModel model)
         {
             _sessionFactory = sessionFactory;
             _model = model;
+            _parser = new QueryParser(sessionFactory, model);
         }
 
         public IEnumerable<Query> Parse(ulong collectionId, HttpRequest request)
@@ -34,22 +36,11 @@ namespace Sir.Store
             }
             else
             {
-                var document = new Dictionary<string, object>();
-                var q = request.Query["q"].ToString();
+                var naturalLanguage = request.Query["q"].ToString();
 
-                foreach (var field in fields)
+                foreach (var q in _parser.Parse(collectionId, naturalLanguage, fields, and, or, not))
                 {
-                    document.Add(field, q);
-                }
-
-                foreach (var field in document)
-                {
-                    long keyId;
-
-                    if (_sessionFactory.TryGetKeyId(collectionId, field.Key.ToHash(), out keyId))
-                    {
-                        yield return new Query(keyId, _model.Tokenize((string)field.Value), and, or, not);
-                    }
+                    yield return q;
                 }
             }
         }
@@ -84,6 +75,38 @@ namespace Sir.Store
                     {
                         yield return new Query(keyId, model.Tokenize((string)field.Value), and, or, not);
                     }
+                }
+            }
+        }
+    }
+
+    public class QueryParser
+    {
+        private readonly SessionFactory _sessionFactory;
+        private readonly IStringModel _model;
+
+        public QueryParser(SessionFactory sessionFactory, IStringModel model)
+        {
+            _sessionFactory = sessionFactory;
+            _model = model;
+        }
+
+        public IEnumerable<Query> Parse(ulong collectionId, string q, string[] fields, bool and, bool or, bool not)
+        {
+            var document = new Dictionary<string, object>();
+
+            foreach (var field in fields)
+            {
+                document.Add(field, q);
+            }
+
+            foreach (var field in document)
+            {
+                long keyId;
+
+                if (_sessionFactory.TryGetKeyId(collectionId, field.Key.ToHash(), out keyId))
+                {
+                    yield return new Query(keyId, _model.Tokenize((string)field.Value), and, or, not);
                 }
             }
         }
