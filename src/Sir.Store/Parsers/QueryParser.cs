7cfc2239aba0cea9f51a23d2bcabcfd24c332334
kreeben <marcuslager@gmail.com>
2019-10-08 20:52:13 +0200

    create class file

---
+++ src/Sir.Store/Parsers/QueryParser.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace Sir.Store
+{
+    public class QueryParser
+    {
+        private readonly SessionFactory _sessionFactory;
+        private readonly IStringModel _model;
+
+        public QueryParser(SessionFactory sessionFactory, IStringModel model)
+        {
+            _sessionFactory = sessionFactory;
+            _model = model;
+        }
+
+        public IEnumerable<Query> Parse(ulong collectionId, string q, string[] fields, bool and, bool or, bool not)
+        {
+            var document = new Dictionary<string, object>();
+
+            foreach (var field in fields)
+            {
+                document.Add(field, q);
+            }
+
+            foreach (var field in document)
+            {
+                long keyId;
+
+                if (_sessionFactory.TryGetKeyId(collectionId, field.Key.ToHash(), out keyId))
+                {
+                    yield return new Query(keyId, _model.Tokenize((string)field.Value), and, or, not);
+                }
+            }
+        }
+    }
+}
