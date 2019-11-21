57ce6546fc2195fbd47e17904c6267555a64541d
Marcus Lager <marcus.lager@avensiastorefront.com>
2019-11-21 12:47:50 +0100

    create crawl page

---
+++ src/Sir.HttpServer/Controllers/CrawlController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sir.HttpServer.Controllers
+{
+    [Route("crawl")]
+    public class CrawlController : UIController
+    {
+        public CrawlController(IConfigurationProvider config, ISessionFactory sessionFactory) : base(config, sessionFactory)
+        {
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return View();
+        }
+    }
+}
