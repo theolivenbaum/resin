11e9769272c1e0aa603ec76673d095e075274360
Marcus Lager <marcus.lager@avensiastorefront.com>
2020-03-25 20:20:20 +0100

    readability

--- src/Sir.StringCompare/Program.cs
+++ src/Sir.StringCompare/Program.cs
@@ -11,49 +11,62 @@ namespace Sir.StringCompare
         {
             var model = new BocModel();
 
-            if (args[0] == "-b" || args[0] == "--build-graph")
+            if (args[0] == "--build-graph")
             {
-                var root = new VectorNode();
-
-                while (true)
-                {
-                    Console.WriteLine("enter word:");
-
-                    var command = Console.ReadLine();
-
-                    if (string.IsNullOrWhiteSpace(command))
-                    {
-                        break;
-                    }
-
-                    var node = new VectorNode(model.Tokenize(command.ToCharArray()).First());
-                    GraphBuilder.TryMerge(root, node, model, model.FoldAngle, model.IdenticalAngle, out _);
-                }
-
-                Console.WriteLine(root.Visualize());
-
-                while (true)
-                {
-                    Console.WriteLine("enter query:");
-
-                    var command = Console.ReadLine();
-
-                    if (string.IsNullOrWhiteSpace(command))
-                    {
-                        break;
-                    }
-
-                    var hit = PathFinder.ClosestMatch(root, model.Tokenize(command.ToCharArray()).First(), model);
-
-                    Console.WriteLine($"{hit.Score} {hit.Node}");
-                }
+                RunInteractiveGraphBuilder(model);
             }
             else
             {
-                var doc1 = new VectorNode(model.Tokenize(args[0].ToCharArray()).First());
-                var doc2 = new VectorNode(model.Tokenize(args[1].ToCharArray()).First());
-                var angle = model.CosAngle(doc1.Vector, doc2.Vector);
-                Console.WriteLine($"similarity: {angle}");
+                Compare(args[0], args[1], model);
+            }
+        }
+
+        private static void Compare(string first, string second, IStringModel model)
+        {
+            var doc1 = new VectorNode(model.Tokenize(first.ToCharArray()).First());
+            var doc2 = new VectorNode(model.Tokenize(second.ToCharArray()).First());
+
+            var angle = model.CosAngle(doc1.Vector, doc2.Vector);
+
+            Console.WriteLine($"similarity: {angle}");
+        }
+
+        private static void RunInteractiveGraphBuilder(IStringModel model)
+        {
+            var root = new VectorNode();
+
+            while (true)
+            {
+                Console.WriteLine("enter text:");
+
+                var command = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(command))
+                {
+                    break;
+                }
+
+                var node = new VectorNode(model.Tokenize(command.ToCharArray()).First());
+
+                GraphBuilder.MergeOrAdd(root, node, model, model.FoldAngle, model.IdenticalAngle);
+            }
+
+            Console.WriteLine(root.Visualize());
+
+            while (true)
+            {
+                Console.WriteLine("enter query:");
+
+                var command = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(command))
+                {
+                    break;
+                }
+
+                var hit = PathFinder.ClosestMatch(root, model.Tokenize(command.ToCharArray()).First(), model);
+
+                Console.WriteLine($"{hit.Score} {hit.Node}");
             }
         }
     }
