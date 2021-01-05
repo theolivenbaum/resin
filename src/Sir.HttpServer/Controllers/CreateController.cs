3f6be67ecfec997eb5890d2b4a2b5f369d1d5fb8
kreeben <marcuslager@gmail.com>
2021-01-05 14:05:22 +0100

    add "create index" controller

---
+++ src/Sir.HttpServer/Controllers/CreateController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sir.Search;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sir.HttpServer.Controllers
+{
+    public class CreateController : UIController
+    {
+        public CreateController(IConfigurationProvider config, SessionFactory sessionFactory) : base(config, sessionFactory)
+        {
+        }
+
+        [HttpPost("/create")]
+        public ActionResult Index([FromForm]CreateModel model)
+        {
+            return View();
+        }
+    }
+
+    public class CreateModel
+    {
+        [FromForm(Name = "sitelist")]
+        public IList<string> SiteList { get; set; }
+    }
+}
