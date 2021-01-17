f0d03a09c95cb1b06aaa7a45b7726643cf238a7b
kreeben <marcuslager@gmail.com>
2021-01-17 20:54:47 +0100

    add webmanifest

--- src/Sir.HttpServer/Controllers/SearchController.cs
+++ src/Sir.HttpServer/Controllers/SearchController.cs
@@ -1,4 +1,6 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
+using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Sir.Search;
@@ -10,6 +12,7 @@ namespace Sir.HttpServer.Controllers
     {
         private readonly IHttpReader _reader;
         private readonly IModel<string> _model;
+        private readonly IConfigurationProvider _config;
 
         public SearchController(
             IHttpReader reader, 
@@ -19,6 +22,7 @@ namespace Sir.HttpServer.Controllers
         {
             _reader = reader;
             _model = model;
+            _config = config;
         }
 
         [HttpGet("/search/")]
@@ -26,6 +30,24 @@ namespace Sir.HttpServer.Controllers
         public async Task<IActionResult> Index(string q, string queryId)
         {
             if (string.IsNullOrWhiteSpace(queryId)) return NotFound();
+
+            var userDirectory = Path.Combine(_config.Get("user_dir"), queryId);
+            var fileName = Path.Combine(userDirectory, $"{"url".ToHash()}.docs");
+
+            if (System.IO.File.Exists(fileName))
+            {
+                var fi = new FileInfo(fileName);
+                var created = fi.CreationTimeUtc;
+                var age = (int)DateTime.Now.Subtract(created).TotalDays;
+                var expiresInDays = 30 - age;
+
+                ViewData["index_expires_in_days"] = expiresInDays;
+            }
+            else
+            {
+                return NotFound();
+            }
+
             if (string.IsNullOrWhiteSpace(q)) return View();
 
             var timer = Stopwatch.StartNew();
